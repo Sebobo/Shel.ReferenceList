fb74c3dcaa75dc6cd186b538922ae57203a0766b
Sebastian Helzle <sebastian@helzle.it>
2025-05-26 09:04:11 +0200

    BUGFIX: Make css classnames unique

--- packages/ReferenceListView/esbuild.js
+++ packages/ReferenceListView/esbuild.js
@@ -1,23 +1,33 @@
 const esbuild = require('esbuild');
-const extensibilityMap = require("@neos-project/neos-ui-extensibility/extensibilityMap.json");
+const extensibilityMap = require('@neos-project/neos-ui-extensibility/extensibilityMap.json');
 const isWatch = process.argv.includes('--watch');
+const CssModulesPlugin = require('esbuild-css-modules-plugin');
 
-/** @type {import("esbuild").BuildOptions} */
+/** @type {import('esbuild').BuildOptions} */
 const options = {
-    logLevel: "info",
+    logLevel: 'info',
     bundle: true,
     minify: !isWatch,
-    target: "es2020",
-    entryPoints: {"Plugin": "src/index.js"},
+    target: 'es2020',
+    entryPoints: { 'Plugin': 'src/index.js' },
     loader: {
-        ".js": "tsx",
+       '.js': 'tsx'
     },
     alias: extensibilityMap,
-    outdir: "../../Resources/Public/ReferenceListView",
-}
+    outdir: '../../Resources/Public/ReferenceListView',
+    plugins: [
+        CssModulesPlugin({
+            force: true,
+            emitDeclarationFile: false,
+            localsConvention: 'camelCaseOnly',
+            namedExports: true,
+            inject: false
+        })
+    ]
+};
 
 if (isWatch) {
-    esbuild.context(options).then((ctx) => ctx.watch())
+  esbuild.context(options).then((ctx) => ctx.watch());
 } else {
-    esbuild.build(options)
+  esbuild.build(options);
 }
